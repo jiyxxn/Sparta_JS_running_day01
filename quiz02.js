8dd654edf24a41afe7046592ab59920d917be242
jinew <jiyunk@naver.com>
2025-01-06 16:42:55 +0900

    🎉 add : quiz02. 객체 선언해보기

---
+++ quiz02.js
@@ -0,0 +1,17 @@
+// 2. 객체 선언해보기
+
+// 지난 시간에 배웠던 객체(object)를 다시 복습해보며, 아래의 조건을 충족하는 객체를 직접 선언해보세요.
+
+// 자기 자신을 소개하는 객체입니다.
+// 이름, 나이, MBTI 세 가지 키와 값이 포함되어 있어야 합니다.
+//콘솔 창에 이름, 나이, MBTI가 나오도록 console.log() 를 찍어보세요.
+
+const introduceOneself = {
+  name: "김지윤",
+  age: "27",
+  MBTI: "ESTP",
+};
+
+console.log("introduceOneself.name", introduceOneself.name);
+console.log("introduceOneself.age", introduceOneself.age);
+console.log("introduceOneself.MBTI", introduceOneself.MBTI);
